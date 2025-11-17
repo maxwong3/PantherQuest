d3215c54b17ec8258483fb99ef15d2d83486143f
IvanLiu <liu15860750430@gmail.com>
2025-11-17 00:54:33 -0500

    login form

---
+++ app/src/app.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { AuthProvider, useAuth } from "./AuthContext";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import AdminPanel from "./pages/AdminPanel";
+
+
+
+const ProtectedRoute = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+  
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
+
+
+export default function App() {
+    return (
+        <AuthProvider>
+            <Router>
+                <Routes>
+                   <Route path="/map" element={ <ProtectedRoute> <Home /> </ProtectedRoute> } />
+                   <Route path="/admin" element={ <ProtectedRoute> <AdminPanel /> </ProtectedRoute> } />
+                   <Route path="/login" element={<Login />} />
+                   <Route path="/" element={<Navigate to="/login" />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
+    );
+}
---
+++ app/src/pages/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useAuth } from '../AuthContext';
+import { useNavigate } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        login(username, password);
+        navigate('/map');
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Username:</label>
+                    <input 
+                        type="text" 
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input 
+                        type="password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
