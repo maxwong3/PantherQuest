dddfb534f9aef086bf01005c2ce3910f0d9868d1
maxwong3 <42724546+maxwong3@users.noreply.github.com>
2025-11-17 21:15:09 -0500

    serve react frontend in express and health check endpoint

--- app/app.js
+++ app/app.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, '../app/build')));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/health', (req, res) =>  {
+    res.json('status: ok')
 })
 
 app.listen(port, () => {
